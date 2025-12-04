dbd48dc6e7523fbc870edbbc0db46c75155a35de
Poojitha Malapati <poojitham_prolifico.digital@wrenchpartner.com>
2025-12-04 15:51:42 +0530

    trying to fix js3  5

---
+++ blocks/herobanner/herobanner.js
@@ -0,0 +1,46 @@
+
+export default function decorate(block) {
+  const background = block.querySelector('[data-aue-prop="backgroundImage"] ')?.src;
+  
+ 
+
+const ctaLink = block.querySelector('[data-aue-prop="textContent_cta"]')?.getAttribute('href') || '';
+const ctaLabel = block.querySelector('[data-aue-prop="textContent_ctaText"]')?.innerText || '';
+  // build your original AEM-style hero banner HTML
+  const html = `
+<div class="container responsivegrid hero-banner container--default-width hero-banner--large-height hero-banner--content-left">
+  <div class="hero-banner__main">
+    <div class="hero-banner__img-container">
+      <div class="image">
+        <div class="cmp-image">
+          <picture>
+            <img src="${background}" alt="">
+          </picture>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid wrapper hero-banner__content-container">
+      <div class="wrapper__inner">
+        <div class="hero-banner__content">
+          <div class="banner-content banner-content--no-img">
+            <div class="banner-content__main">
+              <div class="brand-heading">${title}</div>
+              <div class="banner-content__desc">${description}</div>
+              <div class="banner-content__cta">
+                ${ctaLabel && ctaLink ? `
+                <a class="cta button a-button a-button--primary a-button--md" href="${ctaLink}">
+                  ${ctaLabel}
+                </a>
+              ` : ''}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+  // Replace the block content with your real markup
+  block.innerHTML = html;
+}
